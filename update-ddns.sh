bbec19e033ed423e99aa6a5282eb26b42efc4393
Indra Gunawan <feedsbrain@gmail.com>
2022-10-02 01:02:33 +0800

    feat: log patch data

--- update-ddns.sh
+++ update-ddns.sh
@@ -7,13 +7,14 @@ if [[ -z $AUTH_TOKEN || -z $CF_ZONE_ID || -z $CF_RECORD_ID || -z $CF_RECORD_NAME
 else
   DDNS_IP_ADDRESS="$(curl -s ifconfig.me)"
   CF_API_URL="https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$CF_RECORD_ID"
+  CF_PATCH_DATA="{\"type\":\"A\",\"name\":\"$CF_RECORD_NAME\",\"content\":\"$DDNS_IP_ADDRESS\",\"ttl\":300,\"proxied\":false}"
 
   echo "------------------------------------------------"
   echo "Updating DDNS Record to IP: $DDNS_IP_ADDRESS ..."
   echo "API URL: $CF_API_URL"
-  echo "TOKEN: $AUTH_TOKEN"
+  echo "PATCH DATA: $CF_PATCH_DATA"
   echo "------------------------------------------------"
 
-  curl --http1.1 -X PATCH "$CF_API_URL" -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$CF_RECORD_NAME\",\"content\":\"$DDNS_IP_ADDRESS\",\"ttl\":300,\"proxied\":false}"
+  curl --http1.1 -X PATCH "$CF_API_URL" -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" --data $CF_PATCH_DATA
 fi
 
