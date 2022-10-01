482709813662e85fdb8bfe161a057896e4c889b3
Indra Gunawan <feedsbrain@gmail.com>
2022-10-02 01:46:07 +0800

    feat: use generate function

--- update-ddns.sh
+++ update-ddns.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+generate_post_data()
+{
+cat <<EOF
+{
+  "type":"A",
+  "name":"$CF_RECORD_NAME",
+  "content":"$DDNS_IP_ADDRESS",
+  "ttl": 300,
+  "proxied": false
+}
+EOF
+}
+
 if [[ -z $AUTH_TOKEN || -z $CF_ZONE_ID || -z $CF_RECORD_ID || -z $CF_RECORD_NAME ]]; then
   echo "--------------------------------------------"
   echo "Required environment variable is not set ..."
@@ -7,14 +20,12 @@ if [[ -z $AUTH_TOKEN || -z $CF_ZONE_ID || -z $CF_RECORD_ID || -z $CF_RECORD_NAME
 else
   DDNS_IP_ADDRESS="$(curl -s ifconfig.me)"
   CF_API_URL="https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$CF_RECORD_ID"
-  CF_PATCH_DATA="{\"type\":\"A\",\"name\":\"$CF_RECORD_NAME\",\"content\":\"$DDNS_IP_ADDRESS\",\"ttl\":300,\"proxied\":false}"
 
   echo "------------------------------------------------"
   echo "Updating DDNS Record to IP: $DDNS_IP_ADDRESS ..."
   echo "API URL: $CF_API_URL"
-  echo "PATCH DATA: $CF_PATCH_DATA"
   echo "------------------------------------------------"
 
-  curl --http1.1 -X PATCH -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" -H 'Accept: application/json' --data "$CF_PATCH_DATA" "$CF_API_URL"
+  curl --http1.1 -X PATCH -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" -H 'Accept: application/json' --data "$(generate_post_data)" "$CF_API_URL"
 fi
 
